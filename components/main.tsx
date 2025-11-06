ed53af364c72bb91efd36729b581c8d81957bff6
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-11-06 15:59:39 +0000

    update

--- components/main.tsx
+++ components/main.tsx
@@ -13,14 +13,14 @@ export default function Main() {
       </section>
 
       <section>
-        <div className="p-2 md:p-0 mb-6">
+        <div className="p-4 md:p-0 mb-6">
           <p className="text-[#001a48] font-semibold text-[1.4rem] md:text-4xl">
             Insira a chave e o pin que se encontram no seu comprovativo para
             proceder à validação.
           </p>
         </div>
 
-        <form action="" className="px-2 py-2 space-y-4 md:p-0">
+        <form action="" className="px-4 py-2 space-y-4 md:p-0">
           <div className="flex flex-col gap-2">
             <label
               htmlFor="chave"
